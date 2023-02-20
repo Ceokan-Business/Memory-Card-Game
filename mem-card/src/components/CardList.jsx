3119604de0a2bec6a43ef0a821cf048e8191357e
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2023-02-20 08:15:40 +0200

    Start implement card list

--- mem-card/src/components/CardList.jsx
+++ mem-card/src/components/CardList.jsx
@@ -1,2 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card.jsx";
+
+const CardList = (props) => { 
+    const [pressed, setPressed] = useState({});
+    const [cardList, setCardList] = useState([]);
+
+    useEffect( () => { 
+        let cards = [];
+
+        for(let i = 0; i < props.length; i++) { 
+            cards[i] = {name: i, 
+                selected: false, 
+                imageUrl: "https://picsum.photos/200/300", 
+                ID: i }
+        }
+
+        setCardList(cards);
+    }, []);
+
+    const selectCard = (cardToVerify) => { 
+            cardList.forEach( card => { 
+            if(card.ID === cardToVerify.ID) { 
+                setPressed(card);
+            }
+
+            return card;
+        });
+    };
+
+    return (
+        <section className="card-list">
+            {cardList.map(card => { 
+                return (
+                    <article
+                        onClick = { () => { selectCard(card)}}
+                        key= {card.ID}
+                        className = "card" >
+                            <Card name = {card.name} imageUrl = {card.imageUrl}/> 
+                        </article>
+                )
+            })}
+        </section>
+    )
+};
+
+export default CardList;
