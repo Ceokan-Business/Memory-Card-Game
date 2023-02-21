08be23999413723d402b0bed39369349f431684a
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2023-02-21 20:53:49 +0200

    Basic styling

--- mem-card/src/App.jsx
+++ mem-card/src/App.jsx
@@ -1,9 +1,19 @@
 import React,  { useState, useEffect } from "react";
+import CardList from "./components/CardList";
+import "./styles/main.css";
+import "./styles/card.css";
 
 const App = () => { 
 
   return (
-    <section className="app-container"></section>
+    <section className="app-container">
+      <header>
+        <h1>Memory Card</h1>
+        <h2>Level</h2>
+        <p>Correct Responses / Total Responses</p>
+      </header>
+      <CardList length = {3}/>
+    </section>
   )
 };
 
