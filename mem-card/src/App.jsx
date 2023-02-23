3d5d3b1717e1fa87fe2b23cbf59ccf6b01714e5d
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2023-02-23 18:03:32 +0200

    intro page created

--- mem-card/src/App.jsx
+++ mem-card/src/App.jsx
@@ -1,16 +1,42 @@
 import React,  { useState, useEffect } from "react";
 import Levels from "./components/Levels";
-import Level from "./components/Level";
+import './styles/intro.css'
 import "./styles/main.css";
 import "./styles/card.css";
 
 const App = () => { 
+  const [firstPage, setFirstPage] = useState(true);
+
   return (
     <section className="app-container">
-      <header>
-        <h1>Memory Card</h1>
-      </header>
-      <Levels />
+
+      {/* ***************************** */}
+      {/* **** | First Page Game | **** */}
+      {/* ***************************** */}
+      {
+        firstPage && <article>
+          <header className="intro-header">
+            <section className="vertical-bars">
+            <h1 className="intro-h1">Memory Card</h1>
+            <button 
+              onClick={() => { setFirstPage(false) }}
+              className="intro-button">Play</button>
+              </section>
+          </header>
+        </article>
+      }
+
+
+
+      {!firstPage &&  
+        <section className="play-game-page">
+          <header>
+            <h1>Memory Card</h1>
+          </header>
+          <Levels />
+        </section>
+      }
+
     </section>
   )
 };
